20b7b79e27f34b3cab706af0d5e03c6131461e92
Shailaja Singh <149531941+sshailaja03@users.noreply.github.com>
2026-03-27 10:32:51 +0530

    Update profile picture source in About component

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -3,7 +3,7 @@
 import { motion } from "framer-motion";
 import { Code, LayoutTemplate, HeartHandshake, Layers } from "lucide-react";
 import Image from "next/image";
-import profilePic from "../../assets/cartoon_profile.png";
+import profilePic from "../../assets/myImg1.png";
 
 const valueCards = [
   {
